9e6203f0fa6ed17492c910543bea8420e6fa5169
Chris <crimsontide51191@yahoo.com>
2025-03-04 13:39:17 -0500

    urlToFetch changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { getDailyDeal, getRandomGame, formatPrice } from './helpers.js';
 
-const urlToFetch = process.env.NODE_ENV === 'production' ? 'https://game-generator.onrender.com/steam-deals' : 'http://localhost:3000/steam-deals';
+const urlToFetch = window.location.hostname === 'localhost' ? 'http://localhost:3000/steam-deals' : 'https://game-generator.onrender.com/steam-deals';;
 
 
 const nameOfGame = document.createElement('h2')
